2b5b4541866b322a66d80bbeeb78e134b28ac442
Eduardo Vítor <eduardovitor730@gmail.com>
2023-01-07 16:02:39 -0300

    Temporizador finalizado

--- timer_js/js/index.js
+++ timer_js/js/index.js
@@ -5,11 +5,15 @@ btn.addEventListener("click",turnOn);
 function startTimer() {
     var p = document.querySelector("p");
     var timer_value = parseInt(p.textContent);
-    while(timer_value!=0){
-        setTimeout(function(){
-           timer_value--;
-           p.textContent = timer_value.toString(); 
-        }, 100);
+    var interval_id = setInterval(timer,"1000");
+    function timer() { // Fiz assim porque era preciso organizar a lógica em uma função e para acessar os valores acima
+        timer_value--;
+        if(timer_value < 0) {
+            clearInterval(interval_id);
+        }
+        else {
+            p.textContent = timer_value.toString();
+        }
     }
 }
 
